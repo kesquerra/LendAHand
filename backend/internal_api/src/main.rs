40409e2fb803f813efd2d71da56cee95fd25bc0a
Taylor <taylorknoah@gmail.com>
2022-05-22 17:04:23 -0700

    rebased with updated cors commit into main. works!

--- backend/internal_api/src/main.rs
+++ backend/internal_api/src/main.rs
@@ -41,11 +41,12 @@ async fn main() -> io::Result<()> {
     db.seed_data().await;
 
     let host = config.host.clone();
+		info!("Running backend at {}:{}", config.host, config.port); // log server start
 
     // start server at HOST:PORT, persisting Db connection
     HttpServer::new(move || {
-        
-        let cors = Cors::permissive();
+
+				let cors = Cors::permissive();
         
         App::new()
             .app_data(web::Data::new(SessionData {db: db.clone()}))
