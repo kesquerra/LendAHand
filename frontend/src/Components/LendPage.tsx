42fa898ec26436092dc8b7f7f5cf60c7eb132e1b
Taylor <taylorknoah@gmail.com>
2022-05-25 11:36:04 -0700

    Services ---> services

--- frontend/src/Components/LendPage.tsx
+++ frontend/src/Components/LendPage.tsx
@@ -2,7 +2,7 @@ import { Typography, Box, Paper, Button, Table, TableBody, TableRow, TableCell} 
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { LEND, ROUTER_PATHS } from "../Constants";
-import {getLendItems} from "../Services/LendService";
+import {getLendItems} from "../services/LendService";
 import { ItemType } from "../Types/types";
 
 
